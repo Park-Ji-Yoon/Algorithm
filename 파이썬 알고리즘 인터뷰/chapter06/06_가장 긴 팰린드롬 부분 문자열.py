994b0b340ddf3948938eb053f537c2083249e94b
Jiyoon Park <55547842+Park-Ji-Yoon@users.noreply.github.com>
2021-07-28 21:11:06 +0900

    Update 06_가장 긴 팰린드롬 부분 문자열.py

--- 파이썬 알고리즘 인터뷰/chapter06/06_가장 긴 팰린드롬 부분 문자열.py
+++ 파이썬 알고리즘 인터뷰/chapter06/06_가장 긴 팰린드롬 부분 문자열.py
@@ -10,12 +10,10 @@ def longestPalindrome(s) -> str:
   # 해당 사항이 없을 때 빠르게 return
   if len(s) < 2 or s == s[::-1]: # 길이가 2 미만이거나 전체가 팰린드롬인 경우
     return s
-  
   result = ''
   # 슬라이딩 윈도우 우측으로 이동
   for i in range(0, len(s) - 1):
     result = max(result, expand(i, i + 1), expand(i, i + 2), key=len)
-
   return result
 
 print(longestPalindrome("babad"))
